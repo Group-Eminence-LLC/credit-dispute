b2e7e9ed1e4a26d3218a949d84bc371dba32457d
Simeonone <simeon.kengere@gmail.com>
2024-11-21 12:24:19 +0300

    included redis as a caching solution

--- backend/creditDispute/settings.py
+++ backend/creditDispute/settings.py
@@ -173,20 +173,12 @@ CORS_ALLOWED_ORIGINS = [
 RATELIMIT_VIEW = 'authentication.views.rate_limit_exceeded_view'
 
 # Cache configuration
-if IS_GITHUB_ACTIONS:
-    CACHES = {
-        'default': {
-            'BACKEND': 'django_redis.cache.RedisCache',
-            'LOCATION': 'redis://127.0.0.1:6379/1',
-            'OPTIONS': {
-                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            }
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     }
-else:
-    CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-            'LOCATION': 'my_cache_table',
-        }
-    }
+}
